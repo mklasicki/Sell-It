bcf5eb20078b6a2f7bbdd6ee7b77131979a4c180
cklasicki <cklasicki@gmail.com>
2020-04-18 18:08:41 +0200

    Zmiana generowania ID przez bazę z AUTO na IDENTITY (wlaczy wtedy auto-increment na bazie tego atrybutu)

--- src/main/java/com/marcin/domain/Category.java
+++ src/main/java/com/marcin/domain/Category.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 public class Category {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "name")
     private String name;
--- src/main/java/com/marcin/domain/User.java
+++ src/main/java/com/marcin/domain/User.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String username;
     private String password;
