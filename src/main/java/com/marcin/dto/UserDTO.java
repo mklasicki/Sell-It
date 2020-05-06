36a5b22e1cff885d1f8138f0266b828ba3fbc7c2
Marcin <klasicki.m@gmail.com>
2020-05-06 13:22:01 +0200

    Dodanie klasy UserDTO.

---
+++ src/main/java/com/marcin/dto/UserDTO.java
@@ -0,0 +1,32 @@
+package com.marcin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private boolean enabled;
+    private String authority;
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", enabled=" + enabled +
+                ", authority='" + authority + '\'' +
+                '}';
+    }
+}
