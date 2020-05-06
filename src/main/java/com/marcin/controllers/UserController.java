d104a60abe5ae25ce9ecd0f1c40f20f957bd8994
Marcin <klasicki.m@gmail.com>
2020-05-06 20:46:04 +0200

    Dodanie validacji do kontrolera UserController.

--- src/main/java/com/marcin/controllers/UserController.java
+++ src/main/java/com/marcin/controllers/UserController.java
@@ -3,21 +3,26 @@ package com.marcin.controllers;
 
 import com.marcin.dto.UserDTO;
 import com.marcin.facades.UserFacade;
-import com.marcin.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
 
 
 @Controller
 public class UserController {
 
-    private final UserService userService;
     private final UserFacade userFacade;
+    private final Logger log = LoggerFactory.getLogger(UserController.class);
 
-    public UserController(UserService userService, UserFacade userFacade) {
-        this.userService = userService;
+    public UserController(UserFacade userFacade) {
         this.userFacade = userFacade;
        }
 
@@ -27,7 +32,18 @@ public class UserController {
     }
 
     @PostMapping("/saveUser")
-    public String saveClient(@ModelAttribute("userDTO") UserDTO userDTO) {
+    public String saveClient(@Valid @ModelAttribute("userDTO") UserDTO userDTO, BindingResult result) {
+            if(result.hasErrors()) {
+                List<ObjectError> errors = result.getAllErrors();
+                for(ObjectError error : errors) {
+
+                    log.info("Wystąpily błedy podczas wypełniania formularza {}", error);
+
+                }
+
+                return "addUserForm";
+            }
+
             userFacade.registerNewUser(userDTO);
             return "redirect:/main";
         }
--- src/main/java/com/marcin/dto/UserDTO.java
+++ src/main/java/com/marcin/dto/UserDTO.java
@@ -5,6 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -12,9 +17,16 @@ import lombok.Setter;
 public class UserDTO {
 
     private Long id;
+    @NotBlank(message = "To pole nie może być puste")
+    @Size(min=3, max = 15, message = "Liczba znaków musi się zawierać miedzy 3 a 15")
     private String username;
+    @NotBlank(message = "To pole nie może być puste")
+    @Size(min=3, max = 15, message = "Liczba znaków musi się zawierać miedzy 3 a 15")
     private String password;
+    @NotBlank(message = "To pole nie może być puste")
+    @Email(message = "Wprowadzone dane są nieprawidłowe")
     private String email;
+    @AssertTrue(message = "Aby się zarejestrowac musisz zaakceptować regulamin")
     private boolean enabled;
     private String authority;
 
