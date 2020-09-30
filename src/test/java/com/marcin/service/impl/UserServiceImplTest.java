694ce76309d30c19d82eda4062a81649ad1cb6a2
Marcin <klasicki.m@gmail.com>
2020-09-30 14:33:38 +0200

    Added test class for UserService implementation class.

---
+++ src/test/java/com/marcin/service/impl/UserServiceImplTest.java
@@ -0,0 +1,25 @@
+package com.marcin.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void saveUser() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+}
