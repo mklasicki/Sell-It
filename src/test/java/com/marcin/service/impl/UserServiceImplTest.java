dc88dbb77cdf604bdf7553f0f87eb2909da6301a
Marcin <klasicki.m@gmail.com>
2020-09-30 15:04:02 +0200

    Added test methods for UserServiceImplTest class.

--- src/test/java/com/marcin/service/impl/UserServiceImplTest.java
+++ src/test/java/com/marcin/service/impl/UserServiceImplTest.java
@@ -1,25 +1,77 @@
 package com.marcin.service.impl;
 
+import com.marcin.daos.UserRepository;
 import com.marcin.domain.User;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
 
+    private final long USER_ID = 1L;
+
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserServiceImpl userService;
+
     @Test
-    void getAll() {
+    void should_return_list_of_all_the_users() {
+
+        //given
+        List<User> users = generateTestData();
+        when(userRepository.findAll()).thenReturn(users);
+
+        //when
+        List<User> userResultList = userService.getAll();
+
+        //then
+        assertThat(users, hasSize(3));
+        assertNotNull(userResultList);
     }
 
     @Test
-    void saveUser() {
+    void should_add_new_user() {
+
+        //given
+        List<User> users = new ArrayList<>();
+        User user1 = new User(1L, "Marcin", "Klasicki","mar", "pass123", "marcin@klasicki.pl");
+        when(userRepository.save(user1)).thenReturn(user1);
+
+        //when
+        userService.saveUser(user1);
+
+        //then
+        verify(userRepository, times(1)).save(user1);
     }
 
     @Test
-    void findById() {
+    void should_return_user_when_given_id() {
+
+        //given
+        Optional<User> user = Optional.of(new User(1L, "Marcin", "Klasicki","mar", "pass123", "marcin@klasicki.pl"));
+        when(userRepository.findById(USER_ID)).thenReturn(user);
+
+        //when
+        Optional<User> userResult = userService.findById(USER_ID);
+
+        //than
+        assertThat(userResult, equalTo(user));
+        verify(userRepository, times(1)).findById(any());
     }
 
     @Test
