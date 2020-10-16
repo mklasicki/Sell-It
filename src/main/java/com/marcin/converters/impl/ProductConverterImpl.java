2aa01f6f93e49822e199660522f36bd61802aa1f
Marcin <klasicki.m@gmail.com>
2020-10-17 01:17:04 +0200

    Deleted unused import in ProductController

--- src/main/java/com/marcin/converters/impl/ProductConverterImpl.java
+++ src/main/java/com/marcin/converters/impl/ProductConverterImpl.java
@@ -12,7 +12,6 @@ import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartFile;
 
 
 import java.util.List;
