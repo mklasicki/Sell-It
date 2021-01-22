8b126bc40f665422c9dfbbfa4bec04bcb25857ed
Marcin <klasicki.m@gmail.com>
2021-01-22 15:52:12 +0100

    Added search item on the main page function.

--- src/main/java/com/marcin/service/impl/ProductServiceImpl.java
+++ src/main/java/com/marcin/service/impl/ProductServiceImpl.java
@@ -3,11 +3,13 @@ package com.marcin.service.impl;
 import com.marcin.daos.ProductDAO;
 import com.marcin.domain.Product;
 import com.marcin.service.ProductService;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -45,12 +47,22 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public List<Product> findProductByName(String productName) {
-       if (productName == null || productName.trim().isEmpty()) {
-           throw new  NullPointerException("Search field cannot be empty");
+        if (productName == null || productName.trim().isEmpty()) {
+            throw new  NullPointerException("Search field cannot be empty");
+        }
+
+        List<Product> allProducts = productDAO.getProducts();
+        List<Product> resultProducts = new ArrayList<>();
+
+       for (int i = 0; i < allProducts.size(); i++) {
+           if (StringUtils.containsIgnoreCase(allProducts.get(i).getProductName(), productName)) {
+               resultProducts.add(allProducts.get(i));
+           }
        }
-        log.info("Getting product with name {}" , productName);
-        return productDAO.getProducts().stream()
-                .filter(p->p.getProductName().equalsIgnoreCase(productName)).collect(Collectors.toList());
+
+       log.info("Wszystkie produkty {} Ilość znalezionych produktów  {}",allProducts.size(), resultProducts.size());
+
+       return resultProducts;
     }
 
     @Override
--- src/main/webapp/WEB-INF/view/search-result.jsp
+++ src/main/webapp/WEB-INF/view/search-result.jsp
@@ -4,7 +4,7 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
-
+    <link href="<c:url value="/resources/css/style.css" />" rel="stylesheet">
 </head>
 <body>
  <c:forEach var="temp" items="${products}">
