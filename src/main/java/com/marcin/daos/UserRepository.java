eb696bb5c6a64b9b4b5e6c1c3362bd14a0fb98b3
Marcin <klasicki.m@gmail.com>
2021-02-28 13:53:46 +0100

    Added method in user repository to find user by first and last name.

--- src/main/java/com/marcin/daos/UserRepository.java
+++ src/main/java/com/marcin/daos/UserRepository.java
@@ -1,5 +1,7 @@
 package com.marcin.daos;
 
+import java.util.Optional;
+
 import com.marcin.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,5 +9,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByFirstNameAndLastName(String firstName, String lastName);
 
 }
