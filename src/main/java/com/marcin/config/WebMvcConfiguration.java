736692bc5dc8a4e6206089e48219bf2c2e935671
Marcin <klasicki.m@gmail.com>
2021-04-08 11:56:51 +0200

    Added TilesConfigurer in WebMvcConfiguration class.

--- src/main/java/com/marcin/config/WebMvcConfiguration.java
+++ src/main/java/com/marcin/config/WebMvcConfiguration.java
@@ -1,8 +1,12 @@
 package com.marcin.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,6 +20,20 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     public WebMvcConfiguration(StorageProperties storageProperties)
     {
         this.storageProperties = storageProperties;
+    }
+
+    @Bean
+    TilesConfigurer tilesConfigurer() {
+        TilesConfigurer tilesConfigurer = new TilesConfigurer();
+        tilesConfigurer.setDefinitions(new String[] {"/WEB-INF/views/**/tiles.xml"});
+        tilesConfigurer.setCheckRefresh(true);
+        return tilesConfigurer;
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        TilesViewResolver viewResolver = new TilesViewResolver();
+        registry.viewResolver(viewResolver);
     }
 
     @Override
